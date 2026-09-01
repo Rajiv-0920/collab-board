1b8aa2b0fdb815682136dd8f5f7302401d00da0b
Rajiv Kumar <kumarrajiv0920@gmail.com>
2026-09-01 20:31:16 +0530

    chore: verify mongodb atlas connection

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import 'dotenv/config';
 
 const app = express();
 const PORT = 3000;
@@ -8,5 +10,16 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => {
+  connectDB();
   console.log(`Server is listening on the port: ${PORT}`);
 });
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('MongoDB connected successfully');
+  } catch (error) {
+    console.log('MongoDB connection failed');
+    process.exit(1);
+  }
+};
